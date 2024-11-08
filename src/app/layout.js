9f5d5823cc75d919b25623dd76dbb71fd8e1563e
Tony Lam <tony.lam@iki.fi>
2024-11-08 21:37:41 +0200

    feat: enhance layout with new font and responsive container styling

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import localFont from 'next/font/local';
 import './globals.css';
+import { IBM_Plex_Mono } from 'next/font/google';
+import styles from './page.module.css';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -17,25 +19,29 @@ export const metadata = {
   description: 'Unicafe voting app, for voting where to eat',
 };
 
+const IBMFont = IBM_Plex_Mono({ weight: '300', subsets: ['latin'] });
+
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-        <br />
-        <br />
-        <br />
-        <br />
-        <p>
-          <a
-            href='https://www.cs.helsinki.fi/u/tonylam/'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Tony Lam
-          </a>
-        </p>
-        <a href='https://open-meteo.com/'>Weather data by Open-Meteo.com</a>
+      <body className={`${IBMFont.className} `}>
+        <div className={`${styles.container}`}>
+          {children}
+          <br />
+          <br />
+          <br />
+          <br />
+          <p>
+            <a
+              href='https://www.cs.helsinki.fi/u/tonylam/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Tony Lam
+            </a>
+          </p>
+          <a href='https://open-meteo.com/'>Weather data by Open-Meteo.com</a>
+        </div>
       </body>
     </html>
   );
