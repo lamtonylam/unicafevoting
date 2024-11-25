26131272657496cf5d5cce300d03e917bca6f97c
Tony Lam <tony.lam@iki.fi>
2024-11-25 21:30:08 +0200

    refactor: remove unused font imports from layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,17 +3,6 @@ import './globals.css';
 import { IBM_Plex_Mono } from 'next/font/google';
 import styles from './page.module.css';
 
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-});
-
 export const metadata = {
   title: 'Unicafe voting app',
   description: 'Unicafe voting app, for voting where to eat',
